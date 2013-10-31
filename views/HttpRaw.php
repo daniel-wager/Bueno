c697e18909113c2e96554a63f6cbe38262089d6b
dan wager <dan.wager@gmail.com>
2013-10-30 21:54:58 -0500

    improving HttpRaw headers

--- views/HttpRaw.php
+++ views/HttpRaw.php
@@ -1,6 +1,8 @@
 <?php
 header('HTTP/1.0 '.($this->code ?: '200').($this->message ? " {$this->message}" : ''));
-foreach($this->headers ?: array() as $header) {
+foreach(is_array($this->headers) ?: array($this->headers) as $header) {
+	if (!$header)
+		continue;
 	header($header);
 }
 echo $this->content;
