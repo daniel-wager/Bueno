59da1420b70123d2c5c5168df819d45065210a39
dan wager <dan.wager@gmail.com>
2013-11-04 20:02:39 -0600

    better header support

--- views/HttpRaw.php
+++ views/HttpRaw.php
@@ -1,6 +1,6 @@
 <?php
 header('HTTP/1.0 '.($this->code ?: '200').($this->message ? " {$this->message}" : ''));
-foreach(is_array($this->headers) ?: array($this->headers) as $header) {
+foreach(is_array($this->headers) ? $this->headers : array($this->headers) as $header) {
 	if (!$header)
 		continue;
 	header($header);
