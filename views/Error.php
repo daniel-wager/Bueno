6addab031445514ef043d39ad2f6ebcb9911d18a
dan wager <dan.wager@gmail.com>
2013-12-31 14:13:17 -0600

    better scoping error view style to avoid application to unintended elements

--- views/Error.php
+++ views/Error.php
@@ -1,46 +1,57 @@
-<style>
-h1,h2 {
-	font: bold 15px arial;
-	color: #600;
-}
-h2 {
-	font-size: 12px;
-}
-div, p, li, td, pre, label {
+<style type="text/css">
+<!--
+.bueno.error div,
+.bueno.error p,
+.bueno.error li,
+.bueno.error td,
+.bueno.error pre,
+.bueno.error label {
 	font: normal 12px arial;
 	color: #600;
 }
-label {
+.bueno.error h1,
+.bueno.error h2 {
+	font: bold 15px arial;
+	color: #600;
+}
+.bueno.error h2 {
+	font-size: 12px;
+}
+.bueno.error label {
 	font-size: 11px;
 	font-weight: bold;
 	padding-right: 5px;
 }
-div,pre {
+.bueno.error div,
+.bueno.error pre {
 	padding: 15px;
 }
-.trace {
+.bueno.error .trace {
 	font: normal 10px arial;
 	color: #500;
 }
-div.header {
+.bueno.error div.header {
 	white-space: nowrap;
 	text-align: center;
 }
-div.header span {
+.bueno.error div.header span {
 	font: bold 14px arial;
 }
-div.header span span {
+.bueno.error div.header span span {
 	font: normal 11px arial;
 }
+-->
 </style>
-<div class="header">
-	<span>
-		you have problems
-		<br />
-		<span>(but knowing helps ;)</span>
-	</span>
+<div class="bueno error">
+	<div class="header">
+		<span>
+			you have problems
+			<br />
+			<span>(but knowing helps ;)</span>
+		</span>
+	</div>
+	<div>
+		<?=$this->body?>
+	</div>
+	<pre class="trace"><?=$this->trace?></pre>
 </div>
-<div>
-	<?=$this->body?>
-</div>
-<pre class="trace"><?=$this->trace?></pre>
