6bb8a3b2fe6cafc8e19b60cf8722f3078601493f
dan wager <dan.wager@gmail.com>
2014-01-22 15:11:20 -0600

    UniqueException: improved exception message.

--- exceptions/UniqueException.php
+++ exceptions/UniqueException.php
@@ -2,4 +2,10 @@
 namespace bueno\exceptions;
 
 class UniqueException extends \bueno\exceptions\Exception {
+  public function __construct ($name, $value=null) {
+		$message = "{$name} already exists";
+		if (\bueno\Config::isDebug() && $value)
+			$message .= ' value:"'.print_r($value,true)."\" {$this->file}:{$this->line}";
+    parent::__construct($message);
+  }
 }
