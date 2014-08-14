8429e928d82489a6df4aaea427a8987a02c76e4b
dan wager <dan.wager@gmail.com>
2014-08-13 19:35:45 -0500

    Config::isDev() now set by $_SERVER['SERVER_DEV']

--- Bueno.php
+++ Bueno.php
@@ -16,7 +16,7 @@ class Bueno extends \bueno\Object {
 }
 // set default configs
 Config::init();
-Config::setDev(preg_match('/^(?:local\.|dev\.)/',Bueno::getValue('HTTP_HOST',$_SERVER)));
+Config::setDev(Bueno::getValue('SERVER_DEV',$_SERVER,false));
 Config::setDebug(Config::isDev());
 Config::addNamespace('bueno',BUENO_PATH,false);
 Config::setRequestBase(Bueno::getValue('SCRIPT_NAME',$_SERVER));
