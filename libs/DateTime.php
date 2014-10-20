ce24f6e9409646ede5729c855baceb1e8be2ab12
dan wager <dan.wager@gmail.com>
2014-10-20 15:17:33 -0500

    better local DateTimeZone handling

--- libs/DateTime.php
+++ libs/DateTime.php
@@ -3,22 +3,21 @@ namespace bueno\libs;
 use \DateTimeZone;
 use \bueno\Config;
 class DateTime extends \DateTime {
-	private static $localDateTimeZone;
-	private static $universalDateTimeZone;
-	private $myDateTimeZone; 
-	public function __construct ($datetime=null, DateTimeZone $timezone=null) {
-		if (!self::$localDateTimeZone)
-			self::$localDateTimeZone = new DateTimeZone(Config::getTimeZone());
-		if (!self::$universalDateTimeZone)
-			self::$universalDateTimeZone = new DateTimeZone('UTC');
-		parent::__construct($datetime,$timezone);
+	private static $utcDtz;
+	private $myDtz; 
+	public function __construct ($datetime=null, DateTimeZone $myDtz=null) {
+		if (!($this->myDtz = $myDtz))
+			$this->myDtz = new DateTimeZone(Config::getTimeZone());
+		if (!self::$utcDtz)
+			self::$utcDtz = new DateTimeZone('UTC');
+		parent::__construct($datetime,$this->myDtz);
 	}
 	public function switchToLocal () {
-		$this->setTimeZone(self::$localDateTimeZone);
+		$this->setTimeZone($this->myDtz);
 		return $this;
 	}
 	public function switchToUniversal () {
-		$this->setTimeZone(self::$universalDateTimeZone);
+		$this->setTimeZone(self::$utcDtz);
 		return $this;
 	}
 	public function formatLocal ($format) {
