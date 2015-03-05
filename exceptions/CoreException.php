b488acac2b38f5acd366671ef36a61f275ef4bcb
dan wager <dan.wager@gmail.com>
2015-03-05 14:57:06 -0600

    better handling CoreException logging

--- exceptions/CoreException.php
+++ exceptions/CoreException.php
@@ -1,18 +1,17 @@
 <?php
 namespace bueno\exceptions;
 class CoreException extends \bueno\Exception {
-  public $name;
-  public $tokens;
-  public function __construct ($name=null, $tokens=null) {
-    $this->name = $name;
-    $this->tokens = is_array($tokens) ? $tokens : array();
-    parent::__construct("Core::{$name}");
-  }
-  public function __toString () {
-    $info = "";
-    foreach ($this->tokens as $k=>$v)
-	    $info .= (empty($info)?null:', ').ucfirst($k)."={$v}";
-		parent::setInfo($info);
-    return parent::__toString();
-  }
+	public $name;
+	public $tokens;
+	public function __construct ($name=null, $tokens=null) {
+		$message = "Core::{$name}";
+		$this->name = $name;
+		if (($this->tokens = is_array($tokens) ? $tokens : array())) {
+			$logMessage = $message;
+			foreach ($this->tokens as $k=>$v)
+				$logMessage .= (empty($info)?'':"\n\t").ucfirst($k)."={$v}";
+			$this->setLogMessage($logMessage);
+		}
+		parent::__construct($message);
+	}
 }
