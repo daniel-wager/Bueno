5a983dd9c7b9804c1c1bd4158a743fc4dae0308c
dan wager <dan.wager@gmail.com>
2015-03-05 15:13:58 -0600

    CoreExcpetion logging improvements

--- exceptions/CoreException.php
+++ exceptions/CoreException.php
@@ -7,11 +7,10 @@ class CoreException extends \bueno\Exception {
 		$message = "Core::{$name}";
 		$this->name = $name;
 		if (($this->tokens = is_array($tokens) ? $tokens : array())) {
-			$logMessage = "{$message} tokens (";
+			$tokens = '';
 			foreach ($this->tokens as $k=>$v)
-				$logMessage .= (empty($logMessage)?'':', ')."{$k}={$v}";
-			$logMessage .= ')';
-			$this->setLogMessage($logMessage);
+				$tokens .= (empty($tokens)?'':', ')."{$k}={$v}";
+			$this->setLogMessage("{$message} ({$tokens})");
 		}
 		parent::__construct($message);
 	}
