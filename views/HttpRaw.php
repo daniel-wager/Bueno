cb077a5e1d90518451d960f11c26aec9262aca6e
dan wager <dan.wager@gmail.com>
2015-10-28 16:01:11 -0500

    /views/HttpRaw 	+ adding content checks

--- views/HttpRaw.php
+++ views/HttpRaw.php
@@ -1,4 +1,6 @@
 <?php
+if (headers_sent($file,$line))
+	throw new Exception("Output already started file:{$file}[{$line}]");
 header('HTTP/1.0 '.($this->code ?: '200').($this->message ? " {$this->message}" : ''));
 foreach(array_filter(is_array($this->headers) ? $this->headers : array($this->headers)) as $header) {
 	header($header);
