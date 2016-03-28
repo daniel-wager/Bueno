5b3bd2c81f932b4d7689af1a393323364aaabf46
dan wager <dan.wager@gmail.com>
2016-03-28 05:58:56 -0500

    /daos/MySQL 	+ adding DATABASE_ATOM_FORMAT

--- daos/MySQL.php
+++ daos/MySQL.php
@@ -1,8 +1,9 @@
 <?php
 namespace bueno\daos;
-
 class MySQL extends Database {
 	public function __construct ($user, $pass, $name=null, $host=null, $port=null, $socket=null, $charset=null, $persistant=false) {
+		if (!defined('DATABASE_ATOM_FORMAT'))
+			define('DATABASE_ATOM_FORMAT','%Y-%m-%dT%H:%i:%s+00:00');
 		$dsn = "mysql:host={$host}".($port?";port={$port}":'').($name?";dbname={$name}":'').($charset?";charset={$charset}":'');
 		parent::__construct($dsn,$user,$pass,$persistant);
 	}
