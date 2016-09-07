0f676cb1fcd5d30358037c1ad479765c9eaa8937
dan wager <dan.wager@gmail.com>
2016-09-06 21:10:36 -0500

    /exceptions/InvalidFieldException /exceptions/InvalidFormException 	+ these exceptions have been used in almost every project that i have used.  i find them useful enough to add them to core.

---
+++ exceptions/InvalidFieldException.php
@@ -0,0 +1,15 @@
+<?php
+namespace bueno\exceptions;
+class InvalidFieldException extends InvalidException {
+	private $field;
+  public function __construct ($field, $name=null, $value=null, $options=null) {
+  	$this->setField($field);
+  	parent::__construct(($name?:ucfirst($field)),$value,$options);
+  }
+	private function setField ($field) {
+		$this->field = $field;
+	}
+	public function getField () {
+		return $this->field;
+	}
+}
---
+++ exceptions/InvalidFormException.php
@@ -0,0 +1,56 @@
+<?php
+namespace bueno\exceptions;
+class InvalidFormException extends \bueno\Exception {
+	private $exceptions;
+	public function __construct (array $exceptions=null) {
+		$this->exceptions = array();
+		if ($exceptions)
+			foreach ($exceptions as $e)
+				$this->addFieldException($e);
+		parent::__construct('Invalid Form');
+	}
+	public function mapFieldException ($key, InvalidFieldException $e=null, $useArray=false) {
+		if (empty($key) || !is_scalar($key))
+			throw new InvalidException('key',$key,'scalar');
+		if ($e && !($e instanceof InvalidFieldException))
+			throw new InvalidException('exception class',get_class($e),get_class(new InvalidFieldException(null)));
+		if ($useArray && !empty($this->exceptions[$key])) {
+			is_array($this->exceptions[$key])
+				? $this->exceptions[$key][] = $e
+				: $this->exceptions[$key] = array($this->exceptions[$key],$e);
+		} else {
+			$this->exceptions[$key] = $e;
+		}
+	}
+	public function addFieldException (InvalidFieldException $e, $useArray=false) {
+		$this->mapFieldException($e->getField(),$e,$useArray);
+	}
+	public function hasFieldException ($field) {
+		if (empty($field) || !is_scalar($field))
+			throw new InvalidException('field',$field);
+		return !empty($this->exceptions[$field]);
+	}
+	public function hasFieldExceptions () {
+		return count($this->exceptions)>0;
+	}
+	public function getFieldExceptions () {
+		return $this->exceptions;
+	}
+	public function getFieldException ($key) {
+		if (empty($key) || !is_scalar($key))
+			throw new InvalidException('key',$key);
+		return self::getValue($key,$this->exceptions);
+	}
+	public function getFieldMessages () {
+		$messages = array();
+		foreach ($this->exceptions as $e) {
+			if (is_array($e)) {
+				foreach ($e as $ex)
+					$messages[] = $ex->getMessage();
+			} else {
+				$messages[] = $e->getMessage();
+			}
+		}
+		return $messages;
+	}
+}
